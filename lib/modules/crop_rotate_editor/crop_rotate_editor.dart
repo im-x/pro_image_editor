b5a100bab193afac97add1a43457096359adbd27
hm21 <alex.frei@hotmail.ch>
2025-02-01 15:35:00 +0100

    fix(crop_rotate_editor): add checks for empty cropRect and invalid translate values
    Ensure cropRect is not empty and validate translate values to prevent errors.
    
    Related to #236, #237

--- lib/modules/crop_rotate_editor/crop_rotate_editor.dart
+++ lib/modules/crop_rotate_editor/crop_rotate_editor.dart
@@ -919,6 +919,10 @@ class CropRotateEditorState extends State<CropRotateEditor>
   void _setCropRectBounding({
     double? oldScaleAnimationValue,
   }) {
+    if (cropRect.isEmpty) {
+      return;
+    }
+
     if (!_renderedImgSize.isInfinite) {
       bool fitToWidth =
           (cropRect.width + _cropSpaceHorizontal) > _renderedImgSize.width;
@@ -962,6 +966,9 @@ class CropRotateEditorState extends State<CropRotateEditor>
         translate.dx * scaleFactor,
         translate.dy * scaleFactor,
       );
+      if (translate.dx.isNaN || translate.dx.isInfinite) {
+        throw ArgumentError('Hmmm');
+      }
       _setOffsetLimits();
     }
   }
